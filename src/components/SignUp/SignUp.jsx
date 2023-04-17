86b261bf4a727e8dfb367d6d535ce568cb16a1c7
suvobd100 <makbarbd@gmail.com>
2023-04-17 17:17:52 +0600

    added login & sinup

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import './SignUp.css';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
         <div>
-            <h3>Please SignUp</h3>
+            <div className=" hero min-h-screen bg-base-200">
+                <div className="hero-content">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <h1 className='text-3xl font-bold text-center'>SinUp</h1>
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <span className="label-text">Confirm Password</span>
+                                </label>
+                                <input type="password" name='confirm-password' placeholder="confirm-password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">SignUp</button>
+                                <p className='text-xs'>Already have an account? <Link to='/login' className='text-orange-600'>
+                                Login</Link> </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
